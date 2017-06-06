f420550839d70e2820f174d6a0d04d1cef750a80
BrunoSXS <1557903+brunosxs@users.noreply.github.com>
2017-06-06 09:21:49 -0300

    typos

--- examples/cave/World.gd
+++ examples/cave/World.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-
-
 func _ready():
 	Globals.set("chapter", "intro") # At the start, we set the chapter to intro, so the chars will use this chapter as a base. This will be changed depending on our answer to go or not to go adventuring.
 	Globals.set("dialog", get_node("CanvasLayer/dialog")) # We put a global reference to the dialog.
